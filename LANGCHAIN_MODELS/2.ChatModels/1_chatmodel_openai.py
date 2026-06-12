04a33e0757e02f3e1a23cfecc9ec9f0e071a9e75
saurabhpmp <saurabhchauhan1000@gmail.com>
2026-06-12 18:05:23 +0530

    Add OpenAI chat model implementation

---
+++ LANGCHAIN_MODELS/2.ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4", 
+                   temperature=1.0,
+                   max_completion_tokens=10)
+
+result = model.invoke("Suggest me 5 indian male names")
+
+print(result.content)
